14239311dae934f374830291406569b0360c05e1
Lupitisa Mayika chrit-vie <projectchristvie@gmail.com>
2020-01-30 04:07:45 +0100

    Script to download an image from web

--- MyTest1.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import random
-def download_image(url):
-    name = random.randrange(1,100)
-    full_name = str(name) + '.jpg'
-    urllib.request.urlretrieve(url, full_name)
-download_image('https://....')
